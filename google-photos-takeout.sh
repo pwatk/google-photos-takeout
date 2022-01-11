d404b827af039c1a1ba6c9c3e918a0555b9b55d6
pwatk <pwatk@users.noreply.github.com>
2022-01-11 21:06:40 +0000

    MOV != MP4
    Google Takeout is currently exporting MOV files as MP4 files. A broken-mp4.log file will be created if any files need to be re-downloaded from photos.google.com.

--- google-photos-takeout.sh
+++ google-photos-takeout.sh
@@ -1,123 +1,91 @@
 #!/bin/bash
 
-rm ../error.log
-touch ../error.log
-
 if [ -z "$(type -path exiftool)" ] ;then
 	echo "exiftool does not exist"
 	exit 1
 fi
 
-# Fix files missing CreateDate
+rm -rf ../logs
+mkdir ../logs
 
-cat <<- END
-	
-	Fix all json files containing an copy number in the filename
-	
-END
+echo "*** Fix incorrectly named json files ***"
 
+# e.g. IMG_0001.HEIC(1).json -> IMG_0001(1).HEIC.json
 find . -type f -iname "*([[:digit:]]).json" | while read json ;do
 	mv -v "$json" "$(echo "$json" | sed 's/\(.[[:alpha:]]*\)\(([[:digit:]])\)/\2\1/')"
 done
-
-cat <<- END
 	
-	Import CreateDate from json file if required
-	
-END
+echo "*** Import date and time from json file if required ***"
 
 exiftool -progress \
 	-if 'not $CreateDate' \
 	-tagsfromfile %d%F.json '-createdate<photoTakenTimetimestamp' \
 	-d %s -overwrite_original \
-	-efile3 ../error.log -r .
+	-efile3 ../logs/import-json.log .
 
-# Pair and rename live photos
-#
-# This would be more efficient if Google hadn't dumped entire years into single directories
-# and borked the ContentIdentifier strings so they no longer match for some files. 
+# Let the kludge begin
 
-cat <<- END
-	
-	Move files from Apple devices starting with 'IMG_' and ending with a copy number e.g. '(1)'
-	
-	Ignore minor errors regarding ExtractEmbedded option
-	
-END
+echo "*** Fix MOV files with MP4 extensions ***"
+
+exiftool -progress \
+	-if '$make =~ /^Apple/' -if '$mimetype =~ /video\/quicktime/' \
+	'-filename=%d%f.MOV' \
+	-ext mp4 -efile ../logs/rename-mp4.log .
+
+echo "*** Move files from an Apple device ending with a copy number e.g. '(1)' ***"
 
 exiftool -progress \
 	-if '$filename =~ /^IMG_.*\([[:digit:]]\)/' -if '$make =~ /^Apple/' \
 	'-filename<../output/${createdate#;DateFmt("%Y/%Y-%m-%d")}/IMG_${model;s/ /-/g}-${filename;s/IMG_//;s/\([[:digit:]]\)//}' \
-	-ext mov -ext heic -ext jpg -efile3 ../error.log -r .
+	-ext mov -ext heic -ext jpg -efile3 ../logs/move-with-copy-number.log .
 
-cat <<- END
-	
-	Bulk move everything else from an Apple device starting with 'IMG_'
-	
-	Ignore minor errors regarding ExtractEmbedded option
-	
-END
+echo "*** Move reminaing files from an Apple device ***"
 
 exiftool -progress \
 	-if '$filename =~ /^IMG_/' -if '$make =~ /^Apple/' \
 	'-filename<../output/${createdate#;DateFmt("%Y/%Y-%m-%d")}/IMG_${model;s/ /-/g}-${filename;s/IMG_//}' \
-	-ext mov -ext heic -ext jpg -efile3 ../error.log -r .
+	-ext mov -ext heic -ext jpg -efile3 ../logs/move-remaining.log .
 
-cat <<- END
-	
-	Rename MOV files that are presumably part of live photos using CreateDate from matching HEIC or JPG in same directory
-	
-	Ignore minor errors regarding ExtractEmbedded option or missing files
-	
-END
-
+echo "*** Rename MOV files that are part of a live photo using date and time from matching image file ***"
 exiftool -progress \
-	-if '$filename =~ /^IMG_/' -if '$make =~ /^Apple/' \
+	-if '$filename =~ /^IMG_/' -if '$make =~ /^Apple/' -if '$ContentIdentifier' \
 	-tagsfromfile %d%f.JPG '-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
 	-tagsfromfile %d%f.HEIC '-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
-	-ext mov -efile3 ../error.log -r ../output
+	-ext mov -efile3 ../logs/rename-mov.log -r ../output
 
-cat <<- END
-	
-	Rename all remaining files from Apple devices starting with 'IMG_'
-	
-END
+echo "*** Rename all remaining files from Apple devices starting with 'IMG_' ***"
 
 exiftool -progress \
 	-if '$filename =~ /^IMG_/' -if '$make =~ /^Apple/' \
 	'-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
-	-ext mov -ext heic -ext jpg -efile3 ../error.log -r ../output
+	-ext mov -ext heic -ext jpg -efile3 ../logs/rename-remaining.log -r ../output
 
-cat <<- END
-	
-	Fix broken live photos with differing ContentIdentifier strings and incorrect extensions
-	
-	Ignore minor errors regarding ExtractEmbedded option or missing files
-	
-END
+echo "*** Find MP4 files that should be MOV files - redownload from photos.google.com ***"
+
+# If you re-download these files from photos.google.com you will get MOV files 
+exiftool -progress \
+	-if '$make =~ /^Apple/' \
+	-filepath -s3 -q -ext mp4 -r ../output | tee ../logs/broken-mp4.log
+
+echo "*** Fix broken live photos with differing ContentIdentifier strings and incorrect extensions ***"
 
 # There appears to be a direct correlation between live photos having differing ContentIdentifier strings
 # and incorrect file type.
 #
-# This will check for HEIC files that have been converted to JPEG but still have a HEIC extension and then
-# import the ContentIdentifier string from the matching MOV if it exists.
+# This will check for HEIC files that have been converted to JPEG but still have a HEIC extension. Any files
+# found will be renamed and the ContentIdentifier string imported from the matching MOV if it exists.
 #
 # Hopefully this is a working fix because none of these files will match the actual originals in any case
-
-exiftool -if '$filetype =~ /JPEG/' -filepath -s3 -q -ext heic -r ../output | tee -a ../broken.list
-exiftool '-filename<${filename;s/\.[^.]*$/.jpg/}' -@ ../broken.list
-cat ../broken.list | sed 's/\.heic/.jpg/' > ../still-broken.list
-exiftool -progress -tagsfromfile %d%f.mov '-contentidentifier<ContentIdentifier' -overwrite_original -@ ../still-broken.list
+exiftool -if '$mimetype =~ /image\/jpeg/' -filepath -s3 -q -ext heic -r ../output | tee -a ../logs/broken-heic.list
+exiftool '-filename<${filename;s/\.[^.]*$/.jpg/}' -@ ../logs/broken-heic.list
+cat ../logs/broken-heic.list | sed 's/\.heic/.jpg/' > ../logs/broken-jpg.list
+exiftool -progress -tagsfromfile %d%f.mov '-MediaGroupUUID<ContentIdentifier' -overwrite_original -@ ../logs/broken-jpg.list
 
 # Everything else
 
-cat <<- END
-	
-	Bulk move anything else with a CreateDate tag
-	
-END
+echo "***Bulk move anything else with a CreateDate tag ***"
 
 exiftool -progress \
 	-if '$CreateDate' '-filename<CreateDate' \
 	-d ../output/%Y/%Y-%m-%d/%Y%m%d_%H%M%S%%+c.%%le \
-	-efile3 ../error.log -r .
+	-efile3 ../logs/rename-everything-else.log .
