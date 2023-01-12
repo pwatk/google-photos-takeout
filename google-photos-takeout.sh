5fa8994759f07a33c74c88109cf6e22b79e5098a
pwatk <pwatk@users.noreply.github.com>
2023-01-12 21:34:32 +0000

    Preliminary update
    This update is a restructure and bug fix before a complete rewrite.

--- google-photos-takeout.sh
+++ google-photos-takeout.sh
@@ -5,87 +5,134 @@ if [ -z "$(type -path exiftool)" ] ;then
 	exit 1
 fi
 
-rm -rf ../logs
-mkdir ../logs
+photos="$1"
 
-echo "*** Fix incorrectly named json files ***"
+if [ -z "$photos" ] || [ ! -d "$photos" ] ;then
+	echo "$(basename "$0") /path/to/photos"
+	exit 1
+fi
+
+cd "$photos" || exit 1
+
+mkdir -p ../logs || exit 1
+rm ../logs/*.txt
+
+echo "
+1) Fix incorrectly named json files
+" | tee ../logs/01-fix-json.txt
 
 # e.g. IMG_0001.HEIC(1).json -> IMG_0001(1).HEIC.json
 find . -type f -iname "*([[:digit:]]).json" | while read json ;do
 	mv -v "$json" "$(echo "$json" | sed 's/\(.[[:alpha:]]*\)\(([[:digit:]])\)/\2\1/')"
-done
+done \
+	2>&1 | tee -a ../logs/01-fix-json.txt
 	
-echo "*** Import date and time from json file if required ***"
+echo "
+2) Import Create Date tag from json files if required
+" | tee ../logs/02-import-json.txt
 
 exiftool -progress \
 	-if 'not $CreateDate' \
 	-tagsfromfile %d%F.json '-createdate<photoTakenTimetimestamp' \
 	-d %s -overwrite_original \
-	-efile3 ../logs/import-json.log .
+	. \
+	2>&1 | tee -a ../logs/02-import-json.txt
 
 # Let the kludge begin
 
-echo "*** Fix MOV files with MP4 extensions ***"
+echo "
+3) Fix MOV files with MP4 extensions
+" | tee ../logs/03-fix-mov-extensions.txt
 
-exiftool -progress \
-	-if '$make =~ /^Apple/' -if '$mimetype =~ /video\/quicktime/' \
-	'-filename=%d%f.MOV' \
-	-ext mp4 -efile ../logs/rename-mp4.log .
-
-echo "*** Move files from an Apple device ending with a copy number e.g. '(1)' ***"
-
-exiftool -progress \
-	-if '$filename =~ /^IMG_.*\([[:digit:]]\)/' -if '$make =~ /^Apple/' \
-	'-filename<../output/${createdate#;DateFmt("%Y/%Y-%m-%d")}/IMG_${model;s/ /-/g}-${filename;s/IMG_//;s/\([[:digit:]]\)//}' \
-	-ext mov -ext heic -ext jpg -efile3 ../logs/move-with-copy-number.log .
-
-echo "*** Move reminaing files from an Apple device ***"
-
-exiftool -progress \
-	-if '$filename =~ /^IMG_/' -if '$make =~ /^Apple/' \
-	'-filename<../output/${createdate#;DateFmt("%Y/%Y-%m-%d")}/IMG_${model;s/ /-/g}-${filename;s/IMG_//}' \
-	-ext mov -ext heic -ext jpg -efile3 ../logs/move-remaining.log .
-
-echo "*** Rename MOV files that are part of a live photo using date and time from matching image file ***"
-exiftool -progress \
-	-if '$filename =~ /^IMG_/' -if '$make =~ /^Apple/' -if '$ContentIdentifier' \
-	-tagsfromfile %d%f.JPG '-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
-	-tagsfromfile %d%f.HEIC '-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
-	-ext mov -efile3 ../logs/rename-mov.log -r ../output
-
-echo "*** Rename all remaining files from Apple devices starting with 'IMG_' ***"
-
-exiftool -progress \
-	-if '$filename =~ /^IMG_/' -if '$make =~ /^Apple/' \
-	'-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
-	-ext mov -ext heic -ext jpg -efile3 ../logs/rename-remaining.log -r ../output
-
-echo "*** Find MP4 files that should be MOV files - redownload from photos.google.com ***"
-
-# If you re-download these files from photos.google.com you will get MOV files 
 exiftool -progress \
 	-if '$make =~ /^Apple/' \
-	-filepath -s3 -q -ext mp4 -r ../output | tee ../logs/broken-mp4.log
+	-if '$mimetype =~ /video\/quicktime/' \
+	'-filename=%d%f.MOV' \
+	-ext mp4 \
+	. \
+	2>&1 | tee -a ../logs/03-fix-mov-extensions.txt
+	
+echo "
+4) Fix JPG files with HEIC extensions
+" | tee ../logs/04-fix-jpg-extensions.txt
 
-echo "*** Fix broken live photos with differing ContentIdentifier strings and incorrect extensions ***"
+exiftool -progress \
+	-if '$make =~ /^Apple/' \
+	-if '$mimetype =~ /image\/jpeg/' \
+	'-filename=%d%f.JPG' \
+	-ext heic \
+	. \
+	2>&1 | tee -a ../logs/04-fix-jpg-extensions.txt
 
-# There appears to be a direct correlation between live photos having differing ContentIdentifier strings
-# and incorrect file type.
-#
-# This will check for HEIC files that have been converted to JPEG but still have a HEIC extension. Any files
-# found will be renamed and the ContentIdentifier string imported from the matching MOV if it exists.
-#
-# Hopefully this is a working fix because none of these files will match the actual originals in any case
-exiftool -if '$mimetype =~ /image\/jpeg/' -filepath -s3 -q -ext heic -r ../output | tee -a ../logs/broken-heic.list
-exiftool '-filename<${filename;s/\.[^.]*$/.jpg/}' -@ ../logs/broken-heic.list
-cat ../logs/broken-heic.list | sed 's/\.heic/.jpg/' > ../logs/broken-jpg.list
-exiftool -progress -tagsfromfile %d%f.mov '-MediaGroupUUID<ContentIdentifier' -overwrite_original -@ ../logs/broken-jpg.list
+echo "
+5) Move all files from an Apple device that include a copy number e.g. IMG_0001(1).HEIC
+" | tee ../logs/05-move-1.txt
+
+exiftool -progress \
+	-if '$make =~ /^Apple/' \
+	-if '$filename =~ /^IMG_.*\([[:digit:]]\)/' \
+	'-filename<../output/${createdate#;DateFmt("%Y/%Y-%m-%d")}/IMG_${model;s/ /-/g}-${filename;s/IMG_//;s/\([[:digit:]]\)//}' \
+	-ext heic \
+	-ext jpg \
+	-ext mov \
+	-ext mp4 \
+	. \
+	2>&1 | tee -a ../logs/05-move-1.txt
+
+echo "
+5) Move reminaing files from an Apple device
+" | tee ../logs/05-move-2.txt
+
+exiftool -progress \
+	-if '$make =~ /^Apple/' \
+	-if '$filename =~ /^IMG_/' \
+	'-filename<../output/${createdate#;DateFmt("%Y/%Y-%m-%d")}/IMG_${model;s/ /-/g}-${filename;s/IMG_//}' \
+	-ext heic \
+	-ext jpg \
+	-ext mov \
+	-ext mp4 \
+	. \
+	2>&1 | tee -a ../logs/05-move-2.txt
+
+echo "
+6) Rename video files with a Content Identifier tag using the Create Date tag from the matching photo (if it exists).
+" | tee ../logs/06-rename-live-video.txt
+
+exiftool -progress \
+	-if '$make =~ /^Apple/' \
+	-if '$filename =~ /^IMG_/' \
+	-if '$ContentIdentifier' \
+	-tagsfromfile %d%f.JPG '-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
+	-tagsfromfile %d%f.HEIC '-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
+	-ext mov \
+	-ext mp4 \
+	-r ../output \
+	2>&1 | tee -a ../logs/06-rename-live-video.txt
+
+echo "
+7) Rename all remaining files from an Apple device
+" | tee ../logs/07-rename-remaining-apple.txt
+
+exiftool -progress \
+	-if '$make =~ /^Apple/' \
+	-if '$filename =~ /^IMG_/' \
+	'-filename<${createdate#;DateFmt("%Y%m%d_%H%M%S_")}${filename;s/IMG_//;s/\.[^.]*$//}%+c.%le' \
+	-ext heic \
+	-ext jpg \
+	-ext mov \
+	-ext mp4 \
+	-r ../output \
+	2>&1 | tee -a ../logs/07-rename-remaining-apple.txt
 
 # Everything else
 
-echo "***Bulk move anything else with a CreateDate tag ***"
+echo "
+8) Bulk move everything else with a Create Date tag
+" | tee ../logs/08-everything-else.txt
 
 exiftool -progress \
-	-if '$CreateDate' '-filename<CreateDate' \
+	-if '$CreateDate' \
+	'-filename<CreateDate' \
 	-d ../output/%Y/%Y-%m-%d/%Y%m%d_%H%M%S%%+c.%%le \
-	-efile3 ../logs/rename-everything-else.log .
+	. \
+	2>&1 | tee -a ../logs/08-everything-else.txt
